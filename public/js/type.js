cdba838b9c9dda51b8e0efac85c9ac8018d387ee
AliAbdullah1999 <alithedeveloper1999@gmail.com>
2025-07-23 23:44:51 +0500

    things made better

--- public/js/type.js
+++ public/js/type.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const typed = new Typed('#typed', {
         strings: ['Innovate. Transform. Succeed Together.'],
         typeSpeed: 50,        // Much faster typing (50ms per character)
-        backSpeed: 30,        // Faster backspacing
+        backSpeed: 80,        // Faster backspacing
         startDelay: 1000,      // Small delay before starting
         loop: true,
         showCursor: true,
